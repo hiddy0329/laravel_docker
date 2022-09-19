5004161dfe6fdca6b6df2c39d98e9aabc10261a5
Hidai Satoshi <hiddy19970329@gmail.com>
2022-09-19 21:29:11 +0900

    vueファイルの修正

--- docker-laravel-sample/routes/web.php
+++ docker-laravel-sample/routes/web.php
@@ -29,7 +29,7 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('posts', PostController::class)
+Route::resource('/posts', PostController::class)
     ->only(['index', 'store'])
     ->middleware(['auth', 'verified']);
 
